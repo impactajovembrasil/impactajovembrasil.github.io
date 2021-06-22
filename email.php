860dd40b0f46aa5078e52d26cf8def762efcf58d
luizesquivel05 <62815987+luizesquivel05@users.noreply.github.com>
2021-06-21 23:05:43 -0300

    mexendo no backend do formscontato

--- email.php
+++ email.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-mail enviado</title>
+</head>
+<body>
+
 <?php 
 
 if(isset($_POST(['email']))) && !empty ($_POST (['email']))
@@ -23,3 +33,6 @@ if (mail ( $to, $subject, $body, $header)) {
 
 
 ?>
+
+</body>
+</html>
