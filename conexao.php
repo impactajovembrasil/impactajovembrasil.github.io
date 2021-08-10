8e2c207dcfbca124bf7b598db12712522adeb964
luizesquivel05 <62815987+luizesquivel05@users.noreply.github.com>
2021-08-10 17:02:58 -0300

    conexão com banco de dados - contato

--- conexao.php
+++ conexao.php
@@ -1,10 +1,20 @@
 <?php
 
-$host = "impactajovembrasil.database.windows.net";
-$user = "impacta";
-$pass = "cont@to2021";
-$dbname = "contatoimpacta";
-$port = 3306;
+try {
+    $conn = new PDO("sqlsrv:server = tcp:impactajovembrasil.database.windows.net,1433; Database = contatoimpacta", "impacta", "cont@to2021");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
-$conn = new PDO("sql:host=$host;port=$port;dbname=$dbname", $user, $pass);
+try {
+    $conn = new PDO("sqlsrv:server = tcp:impactajovembrasil.database.windows.net,1433; Database = contatoimpacta", "impacta", "cont@to2021");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 ?>
