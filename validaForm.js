7dd5a14d4f5cacfdc370cd35e96c0b6e0efcf496
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2021-10-11 16:38:19 -0300

    implementando envio de email

---
+++ validaForm.js
@@ -0,0 +1,39 @@
+(function () {
+    var formContact = document.querySelector('#form-contato');
+    var textareaContact = document.querySelector('#form-contato textarea');
+    var textareaVoid = document.querySelector('#form-contato #textareaVoid');
+    var inputName = document.querySelector('#form-contato #nome-contato');
+    var inputNameVoid = document.querySelector('#form-contato #inputNameVoid');
+    var p = document.createElement('p');
+
+
+    p.innerHTML = 'Preencha o campo.';
+    p.style.color = 'red';
+    p.style.fontSize = '12px';
+
+    formContact.addEventListener('submit', function (e) {
+        e.preventDefault();
+        
+        if (!inputName.value) {
+            inputNameVoid.appendChild(p);
+            
+            return;
+        }
+        
+        if (!textareaContact.value) {
+            textareaVoid.appendChild(p);
+            
+            return;
+        }
+        
+        
+        formContact.submit();
+        textareaContact.value = '';
+        document.querySelector('#form-contato input').value = '';
+        document.querySelector('#form-contato #email-contato').value = '';
+
+    });
+})();
+
+
+
