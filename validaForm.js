0ccf5d81d028ad1c82b43a46166c20b55ec7c33a
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2021-10-13 13:21:37 -0300

    update contato input assunto

--- validaForm.js
+++ validaForm.js
@@ -3,6 +3,7 @@
     var textareaContact = document.querySelector('#form-contato textarea');
     var textareaVoid = document.querySelector('#form-contato #textareaVoid');
     var inputName = document.querySelector('#form-contato #nome-contato');
+    var inputSubject = document.querySelector('#form-contato #assunto-contato');
     var inputNameVoid = document.querySelector('#form-contato #inputNameVoid');
     var p = document.createElement('p');
 
@@ -25,10 +26,17 @@
             
             return;
         }
+
+        if (!inputSubject.value) {
+            textareaVoid.appendChild(p);
+            
+            return;
+        }
         
         
         formContact.submit();
         textareaContact.value = '';
+        inputSubject.value = '';
         document.querySelector('#form-contato input').value = '';
         document.querySelector('#form-contato #email-contato').value = '';
 
